1c41e217b4e3a71b5680c0444bfde1fc8d9635d1
hvphu <phuh9057@gmail.com>
2026-09-09 21:15:42 +0700

    Add: reverse-string - submission-8

---
+++ Data Structures & Algorithms/reverse-string/submission-8.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(l==r){
+                break;
+            }
+            swap(s[l],s[r]);
+            ++l;
+            r--;
+        }
+    }
+};
